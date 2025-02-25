d3b93c43f65b24ec2de9e74d9c92f134c4aa8433
Vincent Montminy <2330047@collegebart.ca>
2025-02-25 12:26:20 -0500

    Set up the raylib_start function with the new interface and classes

--- src/entrypoint.cpp
+++ src/entrypoint.cpp
@@ -1,16 +1,37 @@
 #include "entrypoint.h"
 #include "raylib.h"
 #include "knob.h"
+#include "IRC_Interface.h"
+#include "server/server.h"
+
+IRC_Interface* app = NULL;
 
 void raylib_start(void){
+    Color background = RED;
+    #ifdef SERVER
+    app = new Server();
+    #elif defined(CLIENT)
+    background = BLUE;
+    app = new Client();
+    #endif
+
     InitWindow(960,540,"Irc");
     SetTargetFPS(60);
+
+    app->Start("127.0.0.1");
+
     while(!WindowShouldClose()){
-        ClearBackground(RED);
+        ClearBackground(background);
         BeginDrawing();
         //TODO: Do stuff
+
+        app->Update();
+        app->Draw();
+
         EndDrawing();
     }
 
+    app->End();
+    
     CloseWindow();
 }
