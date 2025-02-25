80baed8873a7b44e03254f0a2070d2805ec36645
Vincent Montminy <2330047@collegebart.ca>
2025-02-25 12:23:42 -0500

    Added a client class

---
+++ src/client/client.cpp
@@ -0,0 +1,28 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "client.h"
+
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#define PORT 6667
+
+void Client::Start(const char* url) {
+    
+}
+
+void Client::Update() {
+
+}
+
+void Client::Draw() {
+
+}
+
+void Client::End() {
+
+}
---
+++ src/client/client.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "IRC_Interface.h";
+
+class Client : public IRC_Interface {
+private:
+    
+public:
+    void Start(const char* url) override;
+    void Update() override;
+    void Draw() override;
+    void End() override;
+};
