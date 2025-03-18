031488d8f94a91ae3dd12ef39bcbf05892785cdc
Vincent <121204771+TheLastViny@users.noreply.github.com>
2025-03-18 11:20:44 -0400

    Base pour windows client

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,19 +1,26 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "client.h"
-
 #pragma comment(lib, "ws2_32.lib")
-#include <WinSock2.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>
 #include "imgui.h"
+#include "rlImGui.h"
+#include "raylib.h"
+#include "raymath.h"
+#include "client.h"
 
 #define PORT 6667
 
 void Client::Start(const char* url) {
-    
+    int screenWidth = 1280;
+	int screenHeight = 800;
+
+	SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+	InitWindow(screenWidth, screenHeight, "Projet IRC - Client");
+	SetTargetFPS(144);
+	rlImGuiSetup(true);
+
 }
 
 void Client::Update() {
@@ -22,8 +29,30 @@ void Client::Update() {
 
 void Client::Draw() {
     // Library/rllmGui/example/simple
-    ImGui::Text("Hello, world %d", 123);
-    
+	
+	while (!WindowShouldClose())   
+	{
+		BeginDrawing();
+		ClearBackground(DARKGRAY);
+
+		rlImGuiBegin();
+		static char strNick[256];
+		ImGui::InputTextWithHint("Nicknane", "Input nickname here", strNick, IM_ARRAYSIZE(strNick));
+
+		static char strUser[256];
+		ImGui::InputTextWithHint("Username", "Input username here", strUser, IM_ARRAYSIZE(strUser));
+		//ImGui::SetCursorPos(ImVec2(100,100));
+		ImGui::Button("Connexion", ImVec2(75, 50));
+
+		rlImGuiEnd();
+
+		EndDrawing();
+		
+	}
+	
+	rlImGuiShutdown();
+	CloseWindow();        
+
 }
 
 void Client::End() {
